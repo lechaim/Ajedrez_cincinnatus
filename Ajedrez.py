d6f1729e9491479d2a05b6a71d97b9e21c0ad273
lechaim <lechaimz18@gmail.com>
2019-06-28 12:53:54 -0400

    se agregaron comentarios para discenir el juego

--- Ajedrez.py
+++ Ajedrez.py
@@ -1,8 +1,49 @@
-class Ficha:
-	pass
+class Trebejo:  #Trebejo es el nombre de las piezas con las que se juega ajedrez
+	def __init__ ():
+		pass
 
 class Tablero:
 	pass
 
 
-test
+
+
+
+
+
+
+
+
+
+
+
+
+
+''' -----------------################## INFORMACION DE LOS TREBEJOS #################-----------------------
+Un trebejo tiene forma
+Un trebejo se mueve
+Un trebejo  come
+
+Trebejo (forma, movimiento, direccion en la que come)
+
+rey = R , 1 hacia cualquier cuadro, donde caiga
+dama = D, donde quiera, donde caiga
+torre = T, forma de cruz (vertical y horizontal), donde caiga
+alfil = A, forma de cruz (diagonales), en donde caiga
+caballo = C, forma de L a tres cuadros, encima de donde caiga
+peon = P, si está en posicion inicial puede hacer un paso o dos de lo contrario solo uno
+
+	-----------------################## INFORMACION DE LOS TREBEJOS #################----------------------- '''
+
+#/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
+
+
+
+''' -----------------################## INFORMACION DE LA TABLA #################-----------------------
+
+8x8 lista
+De a-h (horizontal)
+de 1-8 (vertical)
+	-----------------################## INFORMACION DE LA TABLA #################----------------------- '''
+
+
