16dd20a0dd733eb4d18cfb6d4804321057387e37
lechaim <lechaimz18@gmail.com>
2019-06-28 13:06:00 -0400

    Continuacion de las deficiones

--- Ajedrez.py
+++ Ajedrez.py
@@ -2,8 +2,27 @@ class Trebejo:  #Trebejo es el nombre de las piezas con las que se juega ajedrez
 	def __init__ ():
 		pass
 
-class Tablero:
+class Rey(Trebejo):
 	pass
+
+class Reina(Trebejo):
+	pass
+
+class Torrer(Trebejo):
+	pass
+
+class Alfil(Trebejo):
+	pass
+
+class Caballo(Trebejo):
+	pass
+	
+class Peon(Trebejo):
+	pass
+
+class Tablero:
+	def __init__ ():
+		pass
 
 
 
@@ -24,14 +43,14 @@ Un trebejo tiene forma
 Un trebejo se mueve
 Un trebejo  come
 
-Trebejo (forma, movimiento, direccion en la que come)
+Trebejo (forma, movimiento, direccion en la que come, numero de fichas en el juego)
 
-rey = R , 1 hacia cualquier cuadro, donde caiga
-dama = D, donde quiera, donde caiga
-torre = T, forma de cruz (vertical y horizontal), donde caiga
-alfil = A, forma de cruz (diagonales), en donde caiga
-caballo = C, forma de L a tres cuadros, encima de donde caiga
-peon = P, si está en posicion inicial puede hacer un paso o dos de lo contrario solo uno
+rey = R , 1 hacia cualquier cuadro, donde caiga, 1
+dama = D, donde quiera, donde caiga, 1
+torre = T, forma de cruz (vertical y horizontal), donde caiga, 2
+alfil = A, forma de cruz (diagonales), en donde caiga, 2
+caballo = C, forma de L a tres cuadros, encima de donde caiga, 2
+peon = P, si está en posicion inicial puede hacer un paso o dos de lo contrario solo uno, 8
 
 	-----------------################## INFORMACION DE LOS TREBEJOS #################----------------------- '''
 
